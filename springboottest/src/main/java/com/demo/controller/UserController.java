4463171f1eaccb2bac9df39c745d5cb8a11035f8
chenming <477606737@qq.com>
2019-04-15 11:22:21 +0800

    Update UserController.java

--- springboottest/src/main/java/com/demo/controller/UserController.java
+++ springboottest/src/main/java/com/demo/controller/UserController.java
@@ -4,9 +4,7 @@ import com.demo.model.user;
 import com.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,5 +48,12 @@ public class UserController {
         list.add(us2);
         userService.userInfoByList(list);
         return 1;
+    }
+
+
+    @RequestMapping(value = "/test/{username}/{password}" , method = RequestMethod.GET )
+    @ResponseBody
+    public String Test(@PathVariable String username, @PathVariable String password){
+        return "test:"+username+"/"+password;
     }
 }
